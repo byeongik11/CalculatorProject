5a5002b1f3304933e6c3d758a7e61f40d3c5a0ee
byeongik11 <mice2008@naver.com>
2024-05-03 10:40:10 +0900

    feat : 계산기 version1의 입력기능 추가 com.calculate_version1 패키지에 있는 Main클래스에서 계산기를 입력하는 기능을 추가

--- com.sparta.calculator_version1/Main.java
+++ com.sparta.calculator_version1/Main.java
@@ -1,21 +1,49 @@
-package week03.test.step5;
+package test.step5;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Calculator add = new AddOperation();
-        add.calculate(1,2);
-        System.out.println(add.calculate(1,2));
-
-        Calculator subtract = new SubstractOperation();
-        substract.calculate(6,3);
-        System.out.println(subtract.calculate(6,3));
-
+        Calculator subtract = new SubtractOperation();
         Calculator multiply = new MultiplyOperation();
-        multiply.calculate(10,10);
-        System.out.println(multiply.calculate(10,10));
-
         Calculator divide = new DivideOperation();
-        divide.calculate(100,2);
-        System.out.println(divide.calculate(100,2));
+
+        System.out.println("첫 번째 숫자를 입력하세요:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("두 번째 숫자를 입력하세요:");
+        double num2 = scanner.nextDouble();
+
+
+        System.out.println("원하는 연산을 선택하세요:");
+        System.out.println("1. 더하기");
+        System.out.println("2. 빼기");
+        System.out.println("3. 곱하기");
+        System.out.println("4. 나누기");
+        int choice = scanner.nextInt();
+
+        double result = 0;
+        switch (choice) {
+            case 1:
+                result = add.calculate((int)num1, (int)num2);
+                break;
+            case 2:
+                result = subtract.calculate((int)num1, (int)num2);
+                break;
+            case 3:
+                result = multiply.calculate((int)num1, (int)num2);
+                break;
+            case 4:
+                result = divide.calculate((int)num1,(int)num2);
+                break;
+            default:
+                System.out.println("잘못된 선택입니다.");
+                return;
+        }
+
+        System.out.println("결과: " + result);
+
     }
 }
